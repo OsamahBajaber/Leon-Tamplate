9180e6d57e99e47d85e259d605c36860f3469f02
Test <test@Abdullahs-MacBook-Pro-2.local>
2025-02-04 16:07:48 +0300

    adding styles

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+// let burgerList = document.querySelector(".fa-bars");
+// burgerList.onclick = function(){
+//     document.querySelector(".links ul").style.display = "block";
+// }
+// document.querySelector('.header').onclick =  function(){
+//     document.querySelector(".links ul").style.display = "none";
+// }
